2fb37710b5567c0f65b363ea3612bca5d069ec29
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-06-07 22:09:52 +0300

    Added Formulation in Models. Version 1.1

---
+++ Models/Formulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPB_semestral_project_Version2.Models
+{
+    [Serializable]
+    public class Formulation
+    {
+        public int Id { get; set; }
+        public string CountryName { get; set; }
+        public string CityName { get; set; }
+        public double Budget { get; set; }
+        public int QuantityOfPeronsForTour { get; set; }
+    }
+}
---
+++ Services/TourService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPB_semestral_project_Version2.Services
+{
+    public class TourService : TourInterface
+    {
+        // private 
+    }
+}
